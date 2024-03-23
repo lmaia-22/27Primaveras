40ee95aa69bd333ff2d90ca9f449b18fb0e6ca1d
luis-maia-mindprober <luismsm14@gmail.com>
2024-03-23 22:28:27 +0000

    Added google maps finally

--- 27primaveras/src/app/venue/venue.component.ts
+++ 27primaveras/src/app/venue/venue.component.ts
@@ -1,162 +1,96 @@
-import { ChangeDetectorRef, Component, OnInit, OnDestroy, ViewChild } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { CardModule } from 'primeng/card';
-import { GoogleMapsModule } from '@angular/google-maps';
+import { GoogleMapsModule, GoogleMap } from '@angular/google-maps';
 import { TabViewModule } from 'primeng/tabview';
 import { PhotoService } from '../../services/photoService';
-import { GalleriaModule, Galleria } from 'primeng/galleria';
+import { ImageModule } from 'primeng/image';
 
 @Component({
   selector: 'app-venue',
   standalone: true,
-  imports: [CardModule, GoogleMapsModule, TabViewModule, GalleriaModule, Galleria],
+  imports: [CardModule, GoogleMapsModule, TabViewModule, ImageModule],
   templateUrl: './venue.component.html',
   styleUrl: './venue.component.less',
   providers: [
     PhotoService
   ],
 })
-export class VenueComponent implements OnInit, OnDestroy{
+export class VenueComponent implements OnInit, AfterViewInit {
+  @ViewChild(GoogleMap, { static: false })
+  mapComponent!: GoogleMap;
 
-  display: any;
-  center: google.maps.LatLngLiteral = {
-      lat: 41.22709258765494,
-      lng: -8.719402630414645
+  options: google.maps.MapOptions = {
+    center: {lat:  41.22714075429973, lng: -8.719490692415775},
+    zoom: 18,
+    mapTypeId: "satellite",
+    controlSize: 25,
+    scaleControl: true,
+    styles: [
+      { elementType: 'geometry', stylers: [{ color: '#242f3e' }] },
+    ]
   };
-  zoom = 18;
+  directionsService: any
+  directionsRenderer: any
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+    this.directionsService = new google.maps.DirectionsService();
+    this.directionsRenderer = new google.maps.DirectionsRenderer();
+    this.addMarker();
+  }
+
+  addMarker(): void {
+    const center = this.options.center;
+    const markerPosition = { lat: 41.22714075429973, lng: -8.719490692415775 };
+    if (this.mapComponent.googleMap && center) {
+      this.directionsRenderer.setMap(this.mapComponent.googleMap);
+      const marker = new google.maps.Marker({
+        position: center,
+        map: this.mapComponent.googleMap,
+        icon: {
+          url: "https://lmaia-22.github.io/27Primaveras/favicon.ico",
+          scaledSize: new google.maps.Size(25, 25), // scaled size
+          origin: new google.maps.Point(0,0), // origin
+          anchor: new google.maps.Point(0, 0) // anchor
+        }
+      });
+      marker.addListener("click", () => {
+        this.calculateAndDisplayRoute(markerPosition);
+      });
+    }
+  }
+
+  calculateAndDisplayRoute(destination: { lat: number; lng: number; }): void {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition((position) => {
+        const currentPosition = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        };
   
-  
-  images: any[] | undefined;
-
-  showThumbnails: boolean | undefined;
-
-  fullscreen: boolean = false;
-
-  activeIndex: number = 0;
-
-  onFullScreenListener: any;
-
-  @ViewChild('galleria') galleria: Galleria | undefined;
-
-  responsiveOptions: any[] = [
-      {
-          breakpoint: '1024px',
-          numVisible: 5
-      },
-      {
-          breakpoint: '768px',
-          numVisible: 3
-      },
-      {
-          breakpoint: '560px',
-          numVisible: 1
-      }
-  ];
-
-  constructor(
-    private photoService: PhotoService,
-    private cd: ChangeDetectorRef) {}
-
-    ngOnInit() {
-      this.photoService.getRestaurantImages().then((images) => (this.images = images));
-      this.responsiveOptions = [
+        this.directionsService.route(
           {
-              breakpoint: '1024px',
-              numVisible: 5
+            origin: currentPosition,
+            destination: destination,
+            travelMode: google.maps.TravelMode.DRIVING,
           },
-          {
-              breakpoint: '768px',
-              numVisible: 3
-          },
-          {
-              breakpoint: '560px',
-              numVisible: 1
+          (response: any, status: string) => {
+            if (status === google.maps.DirectionsStatus.OK) {
+              this.directionsRenderer.setDirections(response);
+            } else {
+              window.alert('Directions request failed due to ' + status);
+            }
           }
-      ];
-  }
-
-  onThumbnailButtonClick() {
-    this.showThumbnails = !this.showThumbnails;
-  }
-
-  toggleFullScreen() {
-      if (this.fullscreen) {
-          this.closePreviewFullScreen();
-      } else {
-          this.openPreviewFullScreen();
-      }
-
-      this.cd.detach();
-  }
-
-  openPreviewFullScreen() {
-      let elem = this.galleria?.element.nativeElement.querySelector('.p-galleria');
-      if (elem.requestFullscreen) {
-          elem.requestFullscreen();
-      } else if (elem['mozRequestFullScreen']) {
-          /* Firefox */
-          elem['mozRequestFullScreen']();
-      } else if (elem['webkitRequestFullscreen']) {
-          /* Chrome, Safari & Opera */
-          elem['webkitRequestFullscreen']();
-      } else if (elem['msRequestFullscreen']) {
-          /* IE/Edge */
-          elem['msRequestFullscreen']();
-      }
-  }
-
-  onFullScreenChange() {
-      this.fullscreen = !this.fullscreen;
-      this.cd.detectChanges();
-      this.cd.reattach();
-  }
-
-  closePreviewFullScreen() {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if ((document as any).mozCancelFullScreen) { // Use type assertion here
-      (document as any).mozCancelFullScreen();
-    } else if ((document as any).webkitExitFullscreen) { // And here
-      (document as any).webkitExitFullscreen();
-    } else if ((document as any).msExitFullscreen) { // And here
-      (document as any).msExitFullscreen();
+        );
+      }, () => {
+        window.alert('Failed to get your location');
+      });
+    } else {
+      // Browser doesn't support Geolocation
+      window.alert("Geolocation is not supported by this browser.");
     }
   }
   
-
-  bindDocumentListeners() {
-      this.onFullScreenListener = this.onFullScreenChange.bind(this);
-      document.addEventListener('fullscreenchange', this.onFullScreenListener);
-      document.addEventListener('mozfullscreenchange', this.onFullScreenListener);
-      document.addEventListener('webkitfullscreenchange', this.onFullScreenListener);
-      document.addEventListener('msfullscreenchange', this.onFullScreenListener);
-  }
-
-  unbindDocumentListeners() {
-      document.removeEventListener('fullscreenchange', this.onFullScreenListener);
-      document.removeEventListener('mozfullscreenchange', this.onFullScreenListener);
-      document.removeEventListener('webkitfullscreenchange', this.onFullScreenListener);
-      document.removeEventListener('msfullscreenchange', this.onFullScreenListener);
-      this.onFullScreenListener = null;
-  }
-
-  ngOnDestroy() {
-      this.unbindDocumentListeners();
-  }
-
-  galleriaClass() {
-      return `custom-galleria ${this.fullscreen ? 'fullscreen' : ''}`;
-  }
-
-  fullScreenIcon() {
-      return `pi ${this.fullscreen ? 'pi-window-minimize' : 'pi-window-maximize'}`;
-  }
-
-  moveMap(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.center = (event.latLng.toJSON());
-  }
-
-  move(event: google.maps.MapMouseEvent) {
-    if (event.latLng != null) this.display = event.latLng.toJSON();
-  }
 
 }
